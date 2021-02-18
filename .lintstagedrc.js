9d0564aeadc84713bb58e1c975dd2c91a3a9bd86
wuyannan538 <wushuang1591@126.com>
2021-02-18 10:53:50 +0800

    test: test lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 const lint = "eslint";
 const format = "prettier --write";
-const gitAdd = "git add";
 
 module.exports = {
-  "src/**/*.[t|j]s?(x)": [format, lint, gitAdd]
+  "src/**/*.[t|j]s?(x)": [format, lint]
 };
