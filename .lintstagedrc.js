e3746766f1e7fc9001d5f5fd2e0f49cec778d754
wuyannan538 <wushuang1591@126.com>
2021-02-19 17:37:46 +0800

    test: test stylelint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 const lint = "eslint";
+const stylelint = "stylelint";
 const format = "prettier --write";
 
 module.exports = {
-  "src/**/*.[t|j]s?(x)": [format, lint]
+  "src/**/*.[t|j]s?(x)": [format, lint],
+  "src/**/*.s?css": [stylelint]
 };
