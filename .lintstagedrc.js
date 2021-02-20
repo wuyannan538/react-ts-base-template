242c9ed45948955e0991dce4e87c27911bc5edd3
wuyannan538 <wushuang1591@126.com>
2021-02-20 13:07:31 +0800

    feat: support prettier scss

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,8 @@
 const lint = "eslint";
 const stylelint = "stylelint";
-const formatStyle = "stylelint --fix";
 const format = "prettier --write";
 
 module.exports = {
   "src/**/*.[t|j]s?(x)": [format, lint],
-  "src/**/*.[s]css": [formatStyle, stylelint]
+  "src/**/*.[s]css": [format, stylelint]
 };
