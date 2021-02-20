9fceff5941c330fc297771bc6203a1e0e61776e4
wuyannan538 <wushuang1591@126.com>
2021-02-20 10:31:33 +0800

    test: test prettier style

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,9 @@
 const lint = "eslint";
 const stylelint = "stylelint";
+const formatStyle = "stylelint --fix";
 const format = "prettier --write";
 
 module.exports = {
   "src/**/*.[t|j]s?(x)": [format, lint],
-  "src/**/*.[s]css": [stylelint]
+  "src/**/*.[s]css": [formatStyle, stylelint]
 };
